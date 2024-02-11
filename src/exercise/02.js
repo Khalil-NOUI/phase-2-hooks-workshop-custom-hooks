7a9edfc38c155a65c22a223fd0e94ac628ce576b
Khalil-NOUI <NOUI.KHALIL.PR@GMAIL.COM>
2024-02-11 17:17:59 +0100

    Completed assignments

--- src/exercise/02.js
+++ src/exercise/02.js
@@ -2,24 +2,50 @@ import styled from "styled-components";
 import React, { useEffect, useState } from "react";
 
 /* ✅ modify this usePokemon custom hook to take in a query as an argument */
-export function usePokemon() {
-  /* ✅ this hook should only return one thing: an object with the pokemon data */
+export function usePokemon(query) {
+  const [{ data, errors, status }, setPokemon] = useState({
+    data: null,
+    errors: null,
+    status: "idle",
+  });
+
+  useEffect(() => {
+    setPokemon(state => ({ data : null, errors: null, status: "pending" }));
+    fetch(`https://pokeapi.co/api/v2/pokemon/${query}`)
+      .then(r => {
+        if (r.ok) {
+          return r.json();
+        } else {
+          return r.text().then(err => {
+            throw err;
+          });
+        }
+      })
+      .then(data => {
+        setPokemon({ data, errors: null, status: "fulfilled" });
+      })
+      .catch(err => {
+        setPokemon({ data: null, errors: [err], status: "rejected" });
+      });
+  }, [query]);
+
+  return { data, status, errors };
 }
+  /* ✅ this hook should only return one thing: an object with the pokemon data */
+
 
 function Pokemon({ query }) {
+  const {data : pokemon, status, errors} = usePokemon(query)
   /* 
    ✅ move the code from the useState and useEffect hooks into the usePokemon hook
    then, call the usePokemon hook to access the pokemon data in this component
   */
-  const [pokemon, setPokemon] = useState(null);
-  useEffect(() => {
-    fetch(`https://pokeapi.co/api/v2/pokemon/${query}`)
-      .then(r => r.json())
-      .then(setPokemon);
-  }, [query]);
+  
 
   // 🚫 don't worry about the code below here, you shouldn't have to touch it
-  if (!pokemon) return <h3>Loading...</h3>;
+  if (status === "errors") return <h3>{errors[0]}...</h3>;
+
+  if (status !== "fulfilled") return <h3>Loading...</h3>;
 
   return (
     <div>
--- src/exercise/04.js
+++ src/exercise/04.js
@@ -1,34 +1,47 @@
 import { useEffect, useState } from "react";
 
-/* 
-  the two parameters for this function are: 
-  - key: the key on localStorage where we are saving this data
-  - initialValue: the initial value of state
-*/
-export function useLocalStorage(key, initialValue) {
-  /* 
-    ✅ in this hook, use the useState hook. For the initial value for state:
-    use the value saved in localStorage OR the initialValue from the function parameters 
-  */
+function getLocalStorage(key) {
+  let stringValue = localStorage.getItem(key)
+   try {
+    stringValue =  JSON.parse(stringValue)
+} catch {}
+return stringValue }
 
-  /* 
-   ✅ write a useEffect hook 
-   in the useEffect, when state is updated, save the state to localStorage
-   don't forget the dependencies array!
-  */
-  useEffect(() => {});
-
-  /* 
-   ✅ return the same interface as useState:
-   an array with state and a setState function
-  */
-  // 👀 return [state, setState]
+function setLocalStorage(key, value) {
+  const stringValue = JSON.stringify(value);
+  localStorage.setItem(key, stringValue);
 }
+
+
+
+export function useLocalStorage(key, initialValue = null) {
+
+  const [state, setState] = useState(getLocalStorage(key) || initialValue)
+
+  useEffect(() => {
+    setLocalStorage(key, state)
+    }, [key, state]);
+
+    useEffect(() => {
+      function handleChange() {
+        const newValue = getLocalStorage(key);
+        setState(newValue);
+      }
+  
+      window.addEventListener("storage", handleChange);
+  
+      return function cleanup() {
+        window.removeEventListener("storage", handleChange);
+      };
+    }, [key]);
+  
+    return [state, setState];
+  }
 
 function Form() {
   // ✅ after implementing the useLocalStorage hook, replace useState with useLocalStorage
   // don't forget to pass in both arguments (a key and an initialValue)
-  const [name, setName] = useState("");
+  const [name, setName] = useState("", "");
   console.log(name);
 
   return (
